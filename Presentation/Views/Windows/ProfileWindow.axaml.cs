580a1a5c90999a761a63170b8a11af17bbcf92bf
Lera Alex <leraaleksandrova307@gmail.com>
2026-06-02 17:05:15 +0300

    разметка профиля

---
+++ Presentation/Views/Windows/ProfileWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AccountingAndAnalytics.Presentation.Views.Windows;
+
+public partial class ProfileWindow : Window
+{
+    public ProfileWindow()
+    {
+        InitializeComponent();
+    }
+}
